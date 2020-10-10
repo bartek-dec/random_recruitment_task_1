416ad34ee503a055f36d6edf3030c42b8d78a916
Bartek Dec <decbartek@gmail.com>
2020-10-10 20:46:25 +0200

    added initDatabase() method

--- src/main/java/com/example/app/Task1Application.java
+++ src/main/java/com/example/app/Task1Application.java
@@ -1,13 +1,28 @@
 package com.example.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class Task1Application {
 
+    private InitService initService;
+
+    @Autowired
+    public Task1Application(InitService initService) {
+        this.initService = initService;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Task1Application.class, args);
+    }
+
+    @EventListener
+    public void initDatabase(ApplicationReadyEvent event) {
+        initService.initDatabase();
     }
 
 }
