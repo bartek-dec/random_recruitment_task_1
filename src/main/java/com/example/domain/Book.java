6b1a8264c4bf2f59848da9d8baeb60f14a541090
Bartek Dec <decbartek@gmail.com>
2020-10-10 19:45:32 +0200

    added Book entity

---
+++ src/main/java/com/example/domain/Book.java
@@ -0,0 +1,37 @@
+package com.example.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(onlyExplicitlyIncluded = true, callSuper = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@Entity
+@Table(name = "book")
+public class Book extends AbstractEntity<Long> {
+
+    @ToString.Include
+    @EqualsAndHashCode.Include
+    @Column(name = "isbn", unique = true, nullable = false, length = 12)
+    private String ISBN;
+
+    @ToString.Include
+    @Column(name = "title", nullable = false, length = 120)
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "authors_books",
+            joinColumns = {@JoinColumn(name = "book_id")},
+            inverseJoinColumns = {@JoinColumn(name = "author_id")})
+    private List<Author> authors = new ArrayList<>();
+}
