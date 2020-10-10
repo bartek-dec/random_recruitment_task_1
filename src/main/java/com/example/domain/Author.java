c40526214d856ea69c188970d1fdfbeff95597b5
Bartek Dec <decbartek@gmail.com>
2020-10-10 19:45:04 +0200

    added Author entity

---
+++ src/main/java/com/example/domain/Author.java
@@ -0,0 +1,31 @@
+package com.example.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(onlyExplicitlyIncluded = true, callSuper = true)
+@Entity
+@Table(name = "author")
+public class Author extends AbstractEntity<Long> {
+
+    @ToString.Include
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @ToString.Include
+    @Column(name = "surname", nullable = false, length = 50)
+    private String surname;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "authors_books",
+            joinColumns = {@JoinColumn(name = "author_id")},
+            inverseJoinColumns = {@JoinColumn(name = "book_id")})
+    private List<Book> books = new ArrayList<>();
+}
