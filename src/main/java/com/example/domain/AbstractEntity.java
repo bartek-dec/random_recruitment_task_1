94083d2c2b38ece4ace98326a85a8696819f2258
Bartek Dec <decbartek@gmail.com>
2020-10-10 19:08:21 +0200

    added Abstract entity

---
+++ src/main/java/com/example/domain/AbstractEntity.java
@@ -0,0 +1,31 @@
+package com.example.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@MappedSuperclass
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public abstract class AbstractEntity<ID extends Serializable> implements Persistable<ID> {
+
+    @ToString.Include
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private ID id;
+
+    @Override
+    public ID getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return null == getId();
+    }
+}
